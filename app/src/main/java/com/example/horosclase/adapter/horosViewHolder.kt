352dc0d2ef7baf014fa5dcd312960b2893f976fc
AbreuMajanoRoger <140257317+AbreuMajanoRoger@users.noreply.github.com>
2024-06-12 08:13:27 +0200

    Proyecto con imagenes funcionando

--- app/src/main/java/com/example/horosclase/adapter/horosViewHolder.kt
+++ app/src/main/java/com/example/horosclase/adapter/horosViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.horosclase.adapter
 import android.view.View
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.horosclase.horoscopoclass
 import  android.widget.TextView
@@ -8,9 +9,11 @@ import com.example.horosclase.R
 class horosViewHolder(view:View):RecyclerView.ViewHolder(view){
 
     val horosCopo = view.findViewById<TextView>(R.id.horosName)
+    val imgHoroscopo = view.findViewById<ImageView>(R.id.imgHoroscopo)
 
     fun  render(horoscopoclass:horoscopoclass){
         horosCopo.setText(horoscopoclass.name)
+        imgHoroscopo.setImageResource(horoscopoclass.photo)
 
 
     }
--- app/src/main/java/com/example/horosclase/horoscopoProvider.kt
+++ app/src/main/java/com/example/horosclase/horoscopoProvider.kt
@@ -5,18 +5,18 @@ class horoscopoProvider {
 
     companion object {
         val horosList: List<horoscopoclass> = listOf(
-            horoscopoclass("aries", "Aries", 0),
-            horoscopoclass("tauro", "Tauro", 0),
-            horoscopoclass("geminis", "Geminis", 0),
-            horoscopoclass("cancer", "Cancer", 0),
-            horoscopoclass("leo", "Leo", 0),
-            horoscopoclass("virgo", "Virgo", 0),
-            horoscopoclass("libra", "libra", 0),
-            horoscopoclass("escorpio", "Escorpio", 0),
-            horoscopoclass("sagitario", "Sagitario", 0),
-            horoscopoclass("capricornio", "Capricornio", 0),
-            horoscopoclass("acuario", "Acuario", 0),
-            horoscopoclass("piscis", "Piscis", 0),
+            horoscopoclass("aries", "Aries", R.drawable.aries_s),
+            horoscopoclass("tauro", "Tauro", R.drawable.tauro_s),
+            horoscopoclass("geminis", "Geminis",R.drawable.geminis_s),
+            horoscopoclass("cancer", "Cancer", R.drawable.cancer_s),
+            horoscopoclass("leo", "Leo", R.drawable.leo_s),
+            horoscopoclass("virgo", "Virgo", R.drawable.virgo_s),
+            horoscopoclass("libra", "libra", R.drawable.libra_s),
+            horoscopoclass("escorpio", "Escorpio", R.drawable.escorpio_s),
+            horoscopoclass("sagitario", "Sagitario", R.drawable.sagitario_s),
+            horoscopoclass("capricornio", "Capricornio", R.drawable.capricornio_s),
+            horoscopoclass("acuario", "Acuario", R.drawable.acuario_s),
+            horoscopoclass("piscis", "Piscis", R.drawable.piscis_s),
         )
     }
 }
